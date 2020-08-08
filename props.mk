32b7491acfcd8c345ca75b8274553e815ae80ba3
Michael Bestas <mkbestas@lineageos.org>
2020-08-09 01:27:50 +0300

    clean props test
    Change-Id: Ie04fe5d68a222624961d9c7025c6b88e4a685fb3

--- props.mk
+++ props.mk
@@ -2,10 +2,6 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1 \
     audio.deep_buffer.media=true \
-    audio.offload.buffer.size.kb=32 \
-    audio.offload.gapless.enabled=true \
-    audio.safemedia.force=true \
-    av.offload.enable=true \
     persist.vendor.audio.fluence.speaker=true \
     persist.vendor.audio.fluence.voicecall=true \
     persist.vendor.audio.fluence.voicerec=false \
@@ -34,7 +30,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.offload.pstimeout.secs=3 \
     vendor.audio.offload.track.enable=true \
     vendor.audio.parser.ip.buffer.size=262144 \
-    vendor.voice.path.for.pcm.voip=false \
+    vendor.voice.path.for.pcm.voip=true \
     vendor.audio.safx.pbe.enabled=true \
     vendor.audio.tunnel.encode=false \
     vendor.audio.use.sw.alac.decoder=true \
@@ -42,24 +38,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-    bt.max.hfpclient.connections=1 \
-    vendor.bluetooth.soc=cherokee \
     vendor.qcom.bluetooth.soc=cherokee
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    camera.disable_zsl_mode=true \
+    camera.disable_zsl_mode=1 \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap
-
-# CNE
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.cne.feature=1
-
-# Data modules
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.data.df.dev_name=rmnet_usb0 \
-    persist.vendor.data.mode=concurrent \
-    ro.vendor.use_data_netmgrd=true
 
 # Display density
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -67,7 +51,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display post-processing
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qualcomm.cabl=0 \
     ro.vendor.display.cabl=0 \
     vendor.display.enable_default_color_mode=0
 
@@ -82,20 +65,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.egl.hw=0 \
-    debug.sf.enable_hwc_vds=1 \
     debug.sf.latch_unsignaled=0 \
     debug.sf.hw=0 \
     persist.demo.hdmirotationlock=false \
-    persist.sys.sf.native_mode=3 \
     ro.opengles.version=196610
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
     audio.offload.video=true
-
-# Netflix custom property
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.netflix.bsp_rev=Q845-05000-1
 
 # NFC
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -104,102 +81,35 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
-    ro.vendor.qti.core_ctl_min_cpu=2 \
-    ro.vendor.qti.core_ctl_max_cpu=4 \
     ro.vendor.qti.sys.fw.bg_apps_limit=38 \
     vendor.iop.enable_uxe=1
 
-# RCS and IMS
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.dbg.volte_avail_ovr=1 \
-    persist.dbg.vt_avail_ovr=1 \
-    persist.dbg.wfc_avail_ovr=1 \
-    persist.rcs.supported=0
-
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
-    DEVICE_PROVISIONED=1 \
     persist.vendor.radio.apm_sim_not_pwdn=1 \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.rat_on=combine \
     persist.vendor.radio.sib16_support=1 \
-    rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so \
-    ro.com.android.dataroaming=true \
-    ro.telephony.default_network=20,20 \
-    telephony.lteOnCdmaDevice=0
+    rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    config.disable_rtt=true \
-    dev.pm.dyn_samplingrate=1 \
-    media.aac_51_output_enabled=true \
-    media.stagefright.audio.deep=true \
-    media.stagefright.enable-aac=true \
-    media.stagefright.enable-fma2dp=true \
-    media.stagefright.enable-http=true \
-    media.stagefright.enable-player=true \
-    media.stagefright.enable-qcp=true \
-    media.stagefright.enable-scan=true \
-    mm.enable.smoothstreaming=true \
-    mmp.enable.3g2=true \
-    nfc.app_log_level=2 \
-    persist.audio.fluence.speaker=true \
-    persist.audio.fluence.voicecall=true \
-    persist.audio.fluence.voicerec=false \
     persist.backup.ntpServer="0.pool.ntp.org" \
     persist.backup.ntpServer=0.pool.ntp.org \
-    persist.debug.coresight.config=stm-events \
-    persist.debug.wfd.enable=1 \
-    persist.fuse_sdcard=true \
-    persist.mm.enable.prefetch=true \
-    persist.sys.wfd.virtual=0 \
-    persist.timed.enable=true \
     persist.vendor.audio.fluence.audiorec=false \
     persist.vendor.bt.a2dp_offload_cap=sbc-aac \
-    persist.vendor.btstack.a2dp_offload_cap=sbc-aac \
-    persist.vendor.btstack.enable.splita2dp=true \
-    persist.vendor.data.iwlan.enable=true \
-    persist.vendor.overlay.izat.optin=rro \
     persist.vendor.qcomsysd.enabled=1 \
     persist.vendor.qti.games.gt.prof=1 \
-    persist.vendor.radio.atfwd.start=true \
-    qcom.hw.aac.encoder=true \
-    ro.audio.monitorOrientation=true \
-    ro.audio.monitorRotation=true \
-    ro.bluetooth.emb_wp_mode=true \
-    ro.bluetooth.wipower=true \
-    ro.boot.wificountrycode=00 \
     ro.build.shutdown_timeout=6 \
     ro.com.google.gmsversion=9_201907 \
     ro.com.widevine.cachesize=16777216 \
-    ro.config.alarm_vol_default=10 \
-    ro.config.alarm_vol_steps=16 \
-    ro.config.media_vol_default=7 \
-    ro.config.media_vol_steps=18 \
-    ro.config.vc_call_vol_default=6 \
-    ro.config.vc_call_vol_steps=8 \
-    ro.location.osnlp.package=com.google.android.gms \
-    ro.location.osnlp.region.package="" \
-    ro.nfc.port=I2C \
-    ro.oem_unlock_supported=1 \
+    ro.cp_system_other_odex=1 \
     ro.opa.eligible_device=true \
-    ro.qc.sdk.audio.fluencetype=none \
-    ro.qc.sdk.audio.ssr=false \
     ro.setupwizard.mode=OPTIONAL \
-    ro.setupwizard.require_network=junk \
-    ro.setupwizard.rotation_locked=true \
     ro.vendor.at_library=libqti-at.so \
     ro.vendor.bt.bdaddr_path=/sys/module/fih_mfd/parameters/bt_mac \
-    ro.vendor.config.notif_ring_def_vol=4 \
-    ro.vendor.config.notif_ring_vol_steps=13 \
-    ro.vendor.config.notif_ring_def_vol=4 \
-    ro.vendor.config.notif_ring_vol_steps=13 \
-    ro.vendor.qti.cgroup_follow.enable=true \
     ro.wifi.power.reduction=1 \
-    sys.qca1530=detect \
-    tunnel.audio.encode=true \
-    use.voice.path.for.pcm.voip=true \
-    vendor.camera.dual_camera=true \
     vendor.display.disable_fbid_cache=1 \
-    vendor.gatekeeper.disable_spu=true \
-    vendor.perf.iop_v3.enable=1 \
-    vendor.display.res_switch_en=1
+    vendor.gatekeeper.disable_spu=true
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.oem_unlock_supported=1
